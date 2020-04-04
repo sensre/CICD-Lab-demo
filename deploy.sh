8c4894649eea8fb44be060f648038268a9c1e3a3
sensre <32806443+sensre@users.noreply.github.com>
2020-04-04 11:37:31 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 docker kill sencounter redis > /dev/null 2>&1
 docker rm sencounter redis > /dev/null 2>&1
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
 docker-compose up -d
