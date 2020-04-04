0986819d496044b0a171185ef180751ebeac702f
sensre <32806443+sensre@users.noreply.github.com>
2020-04-04 11:42:39 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 docker kill sencounter redis > /dev/null 2>&1
 docker rm sencounter redis > /dev/null 2>&1
-curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
-docker-compose up -d
+
+docker run -d -p 4440:4440 -e RUNDECK_GRAILS_URL=http://172.31.56.113:4440  --name sensrundeck3 -v rundeck:/home/rundeck/data sens/rundeck:3.2.3
+
+
+####curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+####chmod +x /usr/local/bin/docker-compose
+####ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+####docker-compose --version
+####docker-compose up -d
